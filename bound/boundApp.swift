2b0993c1db2230f3123573b3b0b6767e91bf6f3d
MichiKaneko <michihito1005@gmail.com>
2022-12-12 18:34:06 +0900

    Initial Commit

---
+++ bound/boundApp.swift
@@ -0,0 +1,17 @@
+//
+//  boundApp.swift
+//  bound
+//
+//  Created by KANEKO Michihito on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct boundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
